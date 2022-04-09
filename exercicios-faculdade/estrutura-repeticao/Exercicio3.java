2b94a172081faa59ed99d47dff23441350bac16a
carlatakagi <carlatakagi@gmail.com>
2022-04-09 19:05:22 -0400

    add exercise 3

---
+++ exercicios-faculdade/estrutura-repeticao/Exercicio3.java
@@ -0,0 +1,17 @@
+/* Implemente um programa que leia da entrada 10 n´umeros
+inteiros e mostre na sa´ıda quantos s˜ao negativos. */
+import java.util.Scanner;
+
+class Exercicio3 {
+  public static void main(String[] args) {
+    int number;
+
+    Scanner reader = new Scanner(System.in);
+    number = reader.nextInt();
+
+    for(int counter = 0; counter < 10; counter++) {
+      if(number < 0) {
+
+      }
+    }
+}
