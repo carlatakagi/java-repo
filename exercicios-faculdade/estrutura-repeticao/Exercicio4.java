aae3e8c003a634943dba9ba4ac4d40c814da7694
carlatakagi <carlatakagi@gmail.com>
2022-04-10 15:38:13 -0400

    add exercise 4

---
+++ exercicios-faculdade/estrutura-repeticao/Exercicio4.java
@@ -0,0 +1,25 @@
+/*Implemente um programa que leia da entrada as notas de
+prova de 12 estudantes e mostre na sa´ıda a m´edia das provas. */
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+class Exercicio4 {
+  public static void main(String[] args) {
+    int nota, counter;
+    double media, totalNotas = 0;
+
+    Scanner reader = new Scanner(System.in);
+
+    for(counter = 0; counter < 12; counter++) {
+      nota = reader.nextInt();
+      if(nota >= 0) {
+        totalNotas = nota + totalNotas; 
+      }
+      
+    }
+    media = totalNotas / 12;
+    System.out.println(media);
+  }
+}
