80a7ddaa7970741eac182248de729e9c02684077
carlatakagi <carlatakagi@gmail.com>
2022-04-10 23:06:24 -0400

    exercise 6

---
+++ exercicios-faculdade/estrutura-repeticao/Exercicio6.java
@@ -0,0 +1,20 @@
+/*
+Implemente um programa que receba da entrada um n´umero
+inteiro positivo n, calcule e imprima na sa´ıda o valor dos n
+primeiros inteiros positivos.
+*/
+
+import java.util.Scanner;
+
+class Exercicio6 {
+  public static void main(String[] args) {
+    int n; // 5
+
+    Scanner reader = new Scanner(System.in);
+    n = reader.nextInt();
+
+    for (int i = 0; i < n; i++) {
+      
+    }
+  }
+}
