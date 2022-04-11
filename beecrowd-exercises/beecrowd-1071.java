d7c5b2df0461664a68c35374cb97ed0eefddf263
carlatakagi <carlatakagi@gmail.com>
2022-04-10 23:06:16 -0400

    beecrowd 1071

--- beecrowd-exercises/beecrowd-1071.java
+++ beecrowd-exercises/beecrowd-1071.java
@@ -1,3 +1,14 @@
+/*
+Leia 2 valores inteiros X e Y. A seguir, calcule e mostre a soma dos números impares entre eles.
+
+Entrada
+O arquivo de entrada contém dois valores inteiros.
+
+Saída
+O programa deve imprimir um valor inteiro. 
+Este valor é a soma dos valores ímpares que estão entre os valores fornecidos na entrada que deverá caber em um inteiro.
+*/
+
 import java.util.Scanner;
 
 class Main {
