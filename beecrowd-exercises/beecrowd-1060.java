eb120decd7ee4e6c56e5c09016e2f7777eda8917
carlatakagi <carlatakagi@gmail.com>
2022-04-10 23:06:07 -0400

    beecrowd 1060

--- beecrowd-exercises/beecrowd-1060.java
+++ beecrowd-exercises/beecrowd-1060.java
@@ -1,3 +1,11 @@
+/**
+Faça um programa que leia 6 valores. Estes valores serão somente negativos ou positivos (desconsidere os valores nulos). 
+A seguir, mostre a quantidade de valores positivos digitados.
+
+Entrada
+Seis valores, negativos e/ou positivos.
+*/
+
 import java.io.IOException;
 import java.util.Scanner;
 
