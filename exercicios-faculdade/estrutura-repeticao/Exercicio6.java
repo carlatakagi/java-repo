f52c01dc5da336fe0d115f0c351a9d67ee688068
carlatakagi <carlatakagi@gmail.com>
2022-04-11 21:33:14 -0400

    add exercicio 6

--- exercicios-faculdade/estrutura-repeticao/Exercicio6.java
+++ exercicios-faculdade/estrutura-repeticao/Exercicio6.java
@@ -8,13 +8,16 @@ import java.util.Scanner;
 
 class Exercicio6 {
   public static void main(String[] args) {
-    int n; // 5
+    int n, aux = 0; 
 
     Scanner reader = new Scanner(System.in);
     n = reader.nextInt();
 
     for (int i = 0; i < n; i++) {
-      
+      if(n > 0) {
+        i += n;
+        System.out.println(i);
+      }
     }
   }
 }
