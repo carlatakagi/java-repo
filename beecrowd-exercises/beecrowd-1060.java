08d29eb3f49ff3f730b8d6b804a9042be68597d6
carlatakagi <carlatakagi@gmail.com>
2022-04-12 20:29:28 -0400

    1060

--- beecrowd-exercises/beecrowd-1060.java
+++ beecrowd-exercises/beecrowd-1060.java
@@ -1,7 +1,6 @@
 /**
 Faça um programa que leia 6 valores. Estes valores serão somente negativos ou positivos (desconsidere os valores nulos). 
 A seguir, mostre a quantidade de valores positivos digitados.
-
 Entrada
 Seis valores, negativos e/ou positivos.
 */
