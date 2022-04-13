4cbd04273c3e2f56d5c424352d9c0dc8bdfefcb0
carlatakagi <carlatakagi@gmail.com>
2022-04-12 20:57:55 -0400

    beecrowd 1080

--- beecrowd-exercises/beecrowd-1080.java
+++ beecrowd-exercises/beecrowd-1080.java
@@ -1,7 +1,22 @@
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    
-  }
+public class Main {
+
+    public static void main(String[] args) {
+        int num, maior = 0, posicao = 0;
+
+        Scanner reader = new Scanner(System.in);
+        
+        for (int index = 1; index <= 100; index++) {
+            num = reader.nextInt();
+            if (maior > num) {
+                maior = maior;
+                posicao = posicao;
+            } else {
+                posicao = index;
+                maior = num;
+            }
+        }
+        System.out.print(maior + "\n" + posicao + "\n");
+    }
 }
