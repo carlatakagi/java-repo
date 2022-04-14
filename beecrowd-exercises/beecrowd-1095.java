0439ad4df614a893275c71209e4854f21b88443c
carlatakagi <carlatakagi@gmail.com>
2022-04-13 21:51:18 -0400

    beecrowd 1095

--- beecrowd-exercises/beecrowd-1095.java
+++ beecrowd-exercises/beecrowd-1095.java
@@ -1,5 +1,11 @@
+// link do repo https://github.com/ManiruzzamanAkash/URI-Online-Judge-All-Problems-Solution/blob/master/URI_1095.java
+
 class Main {
   public static void main(String[] args) {
-		
+    int I, J;
+
+    for(I = 1, J = 60;J >= 0; I+=3,J-=5){
+			System.out.print("I="+I+" J="+J+"\n");
+		}
 	}
 }
