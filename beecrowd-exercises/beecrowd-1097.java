55a928722875129e0c006964b3d35cfd99f4cb88
carlatakagi <carlatakagi@gmail.com>
2022-04-24 22:44:21 -0400

    1097

---
+++ beecrowd-exercises/beecrowd-1097.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int I;
+
+    /* for(I = 1; I < 10; I += 2) {
+      for(int counterI = I, J = 7; J >= 5; J -= 1) {
+        System.out.println("I=" + counterI + " J=" + J);
+      }
+    } */
+  }
+}
